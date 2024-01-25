ba408251defba772c8652d7cddd04f8991de7fcc
alberto <berto.forjan@gmail.com>
2024-01-25 21:51:03 +0100

    diseño ver 24012024_1

---
+++ app/src/main/java/com/bertosoft/newincidencias/ui/ver/VerViewModel.kt
@@ -0,0 +1,9 @@
+package com.bertosoft.newincidencias.ui.ver
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class VerViewModel @Inject constructor(): ViewModel() {
+}
